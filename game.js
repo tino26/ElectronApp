3acb7ecbd2dd4bf6ec89a04180476f5b9bd10e7c
tinonino <aresfrend13@gmail.com>
2019-02-17 13:40:44 +0200

    first commit

--- game.js
+++ game.js
@@ -1,11 +1,37 @@
 const { app, BrowserWindow } = require('electron')
+var myX=0, myY=500;
+var dy=0, jumping=false;
+
 
 function createWindow () {
   // Create the browser window.
-  let win = new BrowserWindow({ width: 800, height: 600 })
+  let win = new BrowserWindow({ width: 800, height: 600 });
 
   // and load the index.html of the app.
-  win.loadFile('index.html')
+  win.loadFile('index.html');
+}
+app.on('ready', createWindow);
+
+
+function update(){
+	myY-=dy;
+	if(jumping){
+		dy-=0.05;
+	}
+	if(myY>=502){
+		jumping=false;
+		dy=0;
+		myY=500;
+	}
 }
 
-app.on('ready', createWindow)
+function draw(){
+	context.fillRect(myX, myY, 30, 30);
+}
+
+function keyup(key){
+	if(key==32){
+		jumping=true;
+		dy=3;
+	}
+}
